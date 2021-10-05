b640fb1a6f93275b1b00574caed1bba10e01f60a
Nicholas Bush <nickdbush@hotmail.co.uk>
2021-10-05 13:10:24 +0100

    Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import { readdirSync } from "fs";
+
+const manifestIcons = [
+  ...[192, 512].map((size) => ({
+    src: `icons/pwa_icon_x${size}.png`,
+    sizes: `${size}x${size}`,
+    type: "image/png",
+  })),
+  ...[48, 96, 128, 192, 384, 512].map((size) => ({
+    src: `icons/maskable_icon_x${size}.png`,
+    sizes: `${size}x${size}`,
+    type: "image/png",
+    purpose: "maskable",
+  })),
+];
+
+const staticAssets = [
+  "robots.txt",
+  ...readdirSync("public/icons")
+    .filter((file) =>
+      [".svg", ".png", ".xml", ".ico"].some((ext) => file.endsWith(ext))
+    )
+    .map((file) => `icons/${file}`),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ["robots.txt"],
+      includeAssets: staticAssets,
       manifest: {
         name: "Scribbit",
         description: "Pre-flight verifier for The Student",
         theme_color: "#2563EB",
         start_url: "/",
+        icons: manifestIcons,
       },
     }),
   ],
